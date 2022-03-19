4b517b11e0bedafaac99a2e1cc23812a2821440e
--global <--global>
2022-03-19 17:45:42 -0600

    TemplateStrings
    Signed-off-by: --global <--global>

---
+++ js/08concatenacion.js
@@ -0,0 +1,11 @@
+const nombre = 'Goin';
+const email = 'goinbar@correo.com';
+
+// Concatenación
+
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+console.log("Nombre Cliente: " , nombre , " Email: " , email);
+
+
+// Template Strings - String Literals lenguaje JS
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
