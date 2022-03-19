7d3a0e33799f7940c4373b94544e786c1c0d9305
--global <--global>
2022-03-19 17:57:16 -0600

    Booleans
    Signed-off-by: --global <--global>

---
+++ js/09Booleans.js
@@ -0,0 +1,12 @@
+// Booleans
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "false"; // Devuelve como strin (texto)
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(typeof boolean4);
