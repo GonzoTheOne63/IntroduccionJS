0907a59e3ac15e2bb462b336152015f6f86c9d41
--global <--global>
2022-03-19 17:33:19 -0600

    OrdenOperaciones
    Signed-off-by: --global <--global>

---
+++ js/07OrdenOperaciones.js
@@ -0,0 +1,34 @@
+// Orden de las operaciones
+
+let resultado;
+/* let resultado2;
+let resultado3; */
+
+
+resultado = (20 + 30) * 2;
+/* resultado2 = 600 + 600 * 1.05; 
+resultado3 = ( 600 + 600 ) * 1.05;  */
+
+resultado = (100 + 200 +300) * .2;
+
+/* console.log(resultado); */
+
+// Incrementos
+
+let puntaje = 10;
+/* puntaje++; */ //Incrementa en 1
+ 
+/* console.log(puntaje); */
+/* console.log(++puntaje); */
+/* console.log(puntaje++); // Asi devuelve 10
+console.log(puntaje); // Devuelve con incremento de 1 */ 
+/* puntaje++; 10
+++puntaje; 12 */
+puntaje += 10;
+/* console.log(resultado);
+console.log(resultado2);
+console.log(resultado3); */
+console.log(puntaje); //+= Devuelve el valor + lo mismo 
+
+// Decremento
+/* puntaje--; */
