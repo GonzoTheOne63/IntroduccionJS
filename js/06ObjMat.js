a56d67d4aeaca28d814042e2dbb9eef71a1e1573
--global <--global>
2022-03-19 16:19:14 -0600

    ObjetoMath
    Signed-off-by: --global <--global>

---
+++ js/06ObjMat.js
@@ -0,0 +1,24 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado1 = Math.round(2.5); // Redondea hacia arrriba desde .4
+resultado2 = Math.ceil(2.1); // Ceil siempre redondea hacia arriba
+resultado3 = Math.floor(2.9); // Floor siempre redondea hacia abajo
+resultado4 = Math.sqrt(144); // Devuelve a positivo
+resultado5 = Math.min( 3, 5, 1, 8 , 2, 10 ); // Devuelve el menor
+resultado6 = Math.max( 3, 5, 1, 8 , 2, 10 ); // Devuelve el mayor
+resultado7 = Math.random(); // Devuelve aleatorio sin llegar a  1
+resultado8 = Math.floor( Math.random() * 30 ); // Devuelve aleatorio >1
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+console.log(resultado6);
+console.log(resultado7);
+console.log(resultado8);
+console.log(resultado9);
