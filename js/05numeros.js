fe00db6dcca3fccfe53917bb9b104086b4e990b2
--global <--global>
2022-03-19 15:52:06 -0600

    Numeros
    Signed-off-by: --global <--global>

---
+++ js/05numeros.js
@@ -0,0 +1,25 @@
+// Números
+
+const numero = "GoinBar";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 9;
+const numero4 = 3;
+const numero5 = -5;
+const numero6 = .6863;
+
+console.log(numero); // Para JS los números se tratan como tal
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+console.log(numero6);
+// Operaciones aritméticas
+console.log(numero); // Devuelve un string (texto)
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero3 % numero4); // Esta operación devuelve el RESIDUO de la DIVISIÓN
+console.log(numero3 * numero6);
