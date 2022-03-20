962a038992caa9a23ade3e45f40916158ab1d2b1
--global <--global>
2022-03-19 20:21:34 -0600

    RestOperator
    Signed-off-by: --global <--global>

--- js/14Arrays.js
+++ js/14Arrays.js
@@ -1,32 +1,37 @@
-/* // Arreglos o Arrays
+// Arreglos o Arrays
 
-const numeros = [10,20,30,40,50];
-
-
-
+const numeros = [70,20,15,40,50]; // Los "[]" demuestran ser Array
+console.table(numeros);
 
 
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril');
+console.log(meses);
+const meses2 = ['Mayo', 'Junio', 'Julio', 'Agosto'];
+console.table(meses2);
+
+const arreglo = ["Hola", 10, true, "si", null, {nombre: "Goin", ocupacion: "Programador"}, [1, 2, 3, 4, 5]];
+console.log(arreglo);
 
 // Acceder a los valores de un arreglo
 // console.log(numeros[0]);
-// console.log(numeros[1]);
-// console.log(numeros[2]);
-// console.log(numeros[3]);
-// console.log(numeros[4]);
+console.log(numeros[4]);
+console.log(numeros[2]);
+console.log(numeros[5]);
+console.log(numeros[1]);
 
 // Conocer la extensión de un arreglo
-// console.log(meses.length);
+console.log(meses.length);
 
-// numeros.forEach( function(numero) {
-//     console.log(numero);
-// })
+numeros.forEach( function(numero) {
+   console.log(numero);
+}); // forEach automatiza la busqueda
 
 numeros.push(60,70,80); // Al final del arreglo
 numeros.unshift(-10,-20,-30); // al inicio del arreglo
 
 console.table(numeros);
 
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+/* const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']; */
 
 // meses.pop(); // elimina el último elemento
 // meses.shift(); // elimina el primer elemento
@@ -34,8 +39,7 @@ const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
 // meses.splice(2, 1);
 // console.table(meses);
 
-// Rest Operator o Spread Operator
+// RestOperator o Spread Operator
 
 const nuevoArreglo = ['Junio', ...meses];
 console.log(nuevoArreglo);
- */
---
+++ js/14RestOperator.js
@@ -0,0 +1,45 @@
+// Arreglos o Arrays
+
+const numeros = [70,20,15,40,50]; // Los "[]" demuestran ser Array
+console.table(numeros);
+
+
+const meses = new Array('Enero', 'Febrero', 'Marzo', 'Abril');
+console.log(meses);
+/* const meses2 = ['Mayo', 'Junio', 'Julio', 'Agosto'];
+console.table(meses2);
+
+const arreglo = ["Hola", 10, true, "si", null, {nombre: "Goin", ocupacion: "Programador"}, [1, 2, 3, 4, 5]];
+console.log(arreglo);
+
+// Acceder a los valores de un arreglo
+// console.log(numeros[0]);
+console.log(numeros[4]);
+console.log(numeros[2]);
+console.log(numeros[5]);
+console.log(numeros[1]); */  
+
+/* // Conocer la extensión de un arreglo
+console.log(meses.length); */
+
+/* numeros.forEach( function(numero) {
+   console.log(numero);
+}); // forEach automatiza la busqueda
+ */
+numeros.push(60,70,80); // Agrega al final del arreglo
+numeros.unshift(-10,-20,-30); // Agrega al inicio del arreglo
+
+console.table(numeros);
+
+/* const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']; */
+
+meses.pop(); // elimina el último elemento
+meses.shift(); // elimina el primer elemento
+
+meses.splice(2, 1); // Elimina dentro del arreglo 
+console.table(meses);  // Desventaja modifica todo el arreglo
+
+// Rest Operator o Spread Operator para general un NUEVO ARRAY
+
+const nuevoArreglo = [...meses, "Junio"];
+console.log(nuevoArreglo); // No modifica el areglo original
