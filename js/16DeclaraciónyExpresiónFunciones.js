439a3e8720245946e8b529891ba326a87a0bc802
--global <--global>
2022-03-21 09:09:16 -0600

    DeclaraciónyExpresiónFunciones
    Signed-off-by: --global <--global>

---
+++ js/16DeclaraciónyExpresiónFunciones.js
@@ -0,0 +1,22 @@
+// DECLARACIÓN DE LA FUNCIÓN. Son instrucciones o procedimientos que solicitan o implementan una acción.
+function operacion() { // Inincia con 'FUNCTION' y sigue su NOMBRE
+    console.log(10 / 10);  // CUERPO de la función
+    console.log(10 * 10);
+    console.log(10 + 10);
+    console.log(10 - 10);
+}
+operacion();
+
+
+// EXPRESIÓN DE LA FUNCIÓN 
+const calculo = function() { 
+    console.log( 3 + 3);
+    console.log( 3 * 3);
+    console.log( 3 / 3);
+    console.log( 3 - 3);
+}
+calculo();
+
+
+
+
