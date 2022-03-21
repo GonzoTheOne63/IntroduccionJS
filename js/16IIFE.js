fdd2a801addff9e8214b2016a1371ea0b2b65479
--global <--global>
2022-03-21 09:19:35 -0600

    IIFE
    Signed-off-by: --global <--global>

---
+++ js/16IIFE.js
@@ -0,0 +1,4 @@
+// IIFE Es una forma de FUNCIÓN que protege que los datos y declaraaciones se lean en otro archivo 
+(function() {
+  console.log('Esto es función IFEE');
+})(); // Tambien se invoca sola
