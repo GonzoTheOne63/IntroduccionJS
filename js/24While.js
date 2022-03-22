7bb60ab22be4282b69471dfac513a112907f85c7
--global <--global>
2022-03-21 19:02:11 -0600

    WHILE_LOOP
    Signed-off-by: --global <--global>

---
+++ js/24While.js
@@ -0,0 +1,44 @@
+// While Loop evalua mientras la condición sea verdadera
+
+/* let i = 1;  // 'i' = ÍNDICE Comienza en 0, busca menor a 10 y va a incrementar de 1 a 1
+while(i <= 100) { // Primero evalua la CONDICIÓN si es verdadera la ejecuta
+    if(i % 2 === 0) {
+      console.log(`El número ${i} es PAR`)
+    }else {
+      console.log(`El número ${i} es IMPAR`)
+    } 
+    i++; // Equivale el incremento   
+} */
+
+/* const carrito = [
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Televisión 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
+];
+let i = 0;
+while(i < carrito.length) {
+  console.log(carrito[i].nombre),
+  console.log(carrito[i].precio)
+
+  i++;
+}
+ */
+/* let i = 20;
+
+while(i < 10) {
+  console.log(`Desde el While loop`);
+  i++;
+} */
+
+/* let i = 20;
+
+while(i > 10) {
+  console.log(`Desde el While loop`);
+  i++;
+} */
+
